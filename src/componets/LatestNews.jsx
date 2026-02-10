a87ba10e8ded6b47fb1f42fa834b3752648c1bea
Rashikchowdhury <muhammadrashikchowdhury@gmail.com>
2026-02-10 23:13:20 +0600

    showing breking news on marquee

--- src/componets/LatestNews.jsx
+++ src/componets/LatestNews.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { use } from 'react';
 import Marquee from 'react-fast-marquee';
 
+const newsPromise = fetch("/news.json").then(res => res.json())
+
 const LatestNews = () => {
+    const news = use(newsPromise)
+
+    const allBreakingNews = news.filter(news => news.others.is_today_pick == true)
+
+    const breakingNews = allBreakingNews.map(news => news.title + ". ")
+
     return (
         <div className='flex items-center gap-4 bg-base-200 p-3 mt-6'>
             <p className='bg-secondary text-white p-2'>Latest</p>
             <Marquee pauseOnHover={true}>
                 <p className='font-semibold'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia consequuntur autem modi corrupti similique architecto provident dignissimos! Libero, magni alias!                      
+                    {breakingNews}
                 </p>
             </Marquee>
         </div>
